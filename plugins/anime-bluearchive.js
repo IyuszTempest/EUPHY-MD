50cbc0dfdae18dac436d74951e30ee45d4fd4f92
IyuszTempest <aditwibubawang@gmail.com>
2026-02-27 22:44:27 +0700

    Create anime-bluearchive.js

---
+++ plugins/anime-bluearchive.js
@@ -0,0 +1,25 @@
+/**
+ * Euphy-Bot - Blue Archive Random Image
+ * Identitas: Euphylia Magenta Randomizer ✨
+ */
+
+module.exports = {
+    command: ['bluearchive'],
+    category: 'anime',
+    call: async (conn, m) => {
+        try {
+            await conn.sendMessage(m.chat, { react: { text: '📸', key: m.key } });
+            
+            // Mengirim link langsung sebagai gambar
+            const imageUrl = `https://api.siputzx.my.id/api/r/blue-archive`;
+            
+            await conn.sendMessage(m.chat, { 
+                image: { url: imageUrl }, 
+                caption: `🏮 *Blue Archive Random Image* 🏮\n\nNih foto karakternya!` 
+            }, { quoted: m });
+            
+        } catch (e) {
+            m.reply("Gagal mengambil foto, servernya mungkin lagi lelah.");
+        }
+    }
+};
