9508860f61bc940e24e785681c7f199d2664862f
IyuszTempest <aditwibubawang@gmail.com>
2026-04-10 07:52:51 +0700

    Update anime-bluearchive.js

--- plugins/anime-bluearchive.js
+++ plugins/anime-bluearchive.js
@@ -14,14 +14,9 @@ module.exports = {
             // Mengirim link langsung sebagai gambar
             const imageUrl = `https://api.siputzx.my.id/api/r/blue-archive`;
             
-            await conn.sendMessage(m.chat, { 
-                image: { url: imageUrl }, 
-                caption: `🏮 *Blue Archive Random Image* 🏮\n\nNih foto karakternya!` 
-            }, { quoted: m });
-
              // Kirim hasil dengan UI Euphylia Magenta
             await conn.sendMessage(m.chat, { 
-                image: { url: data.result }, 
+                image: { url: imageUrl }, 
                 caption: `🏮 *Blue Archive Random Image* 🏮\n\n👤 *Requester:* @${m.sender.split`@`[0]}`,
                 contextInfo: {
                     mentionedJid: [m.sender],
