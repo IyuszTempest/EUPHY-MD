2c943c70c558749c5a530d79892beaf10ef4f40d
IyuszTempest <aditwibubawang@gmail.com>
2026-04-15 20:39:19 +0700

    Update game-inv.js

--- plugins/game-inv.js
+++ plugins/game-inv.js
@@ -11,8 +11,6 @@ module.exports = {
         let k = user.koleksi;
 
         let daftar = `╭━━〔 🎎 *𝙸𝙽𝚅𝙴𝙽𝚃𝙾𝚁𝚈* 〕━━┓\n┃\n`;
-        if (k.waifu?.length) daftar += `┣ ❤️ *WAIFU:* ${k.waifu.join(', ')}\n`;
-        if (k.husbu?.length) daftar += `┣ 💙 *HUSBU:* ${k.husbu.join(', ')}\n`;
         if (k.figure?.length) daftar += `┣ 🎎 *FIGURE:* ${k.figure.join(', ')}\n`;
         if (k.harian?.length) daftar += `┣ 🛠️ *DAILY:* ${k.harian.join(', ')}\n`;
         
