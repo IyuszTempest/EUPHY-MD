560accaedc5e4a7705e0917e657fd98abe509dcb
Yushima <aditwibubawang@gmail.com>
2026-06-01 06:54:08 +0700

    Update anime-bluearchive.js

--- plugins/anime-bluearchive.js
+++ plugins/anime-bluearchive.js
@@ -9,29 +9,27 @@ module.exports = {
     noPrefix: true,
     call: async (conn, m) => {
         try {
-            await conn.sendMessage(m.chat, { react: { text: '📸', key: m.key } });
+            await conn.sendMessage(m.chat, { react: { text: '🤨', key: m.key } });
             
-            // Mengirim link langsung sebagai gambar
             const imageUrl = `https://api.siputzx.my.id/api/r/blue-archive`;
             
-             // Kirim hasil dengan UI Euphylia Magenta
             await conn.sendMessage(m.chat, { 
                 image: { url: imageUrl }, 
-                caption: `🏮 *Blue Archive Random Image* 🏮\n\n👤 *Requester:* @${m.sender.split`@`[0]}`,
+                caption: `> Requester: @${m.sender.split`@`[0]}`,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: global.idch,
-                        newsletterName: `BA Community - ${global.namech}`
+                        newsletterName: `${global.namech} × Blue Archive`
                     }
                 }
             }, { quoted: m });
 
              } catch (e) {
             console.error(e)
-            m.reply(`error nih`)
+            m.reply(`> error nih`)
         }
     }
 };
