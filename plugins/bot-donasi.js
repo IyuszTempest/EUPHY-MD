dbfc04e1df2cda979dc368d2301671c251fbeff6
Yushima <aditwibubawang@gmail.com>
2026-06-01 08:04:08 +0700

    Update bot-donasi.js

--- plugins/bot-donasi.js
+++ plugins/bot-donasi.js
@@ -4,27 +4,29 @@
  */
 
 module.exports = {
-    command: ['donasi', 'donate', 'sedekah', 'qris'],
+    command: ['donasi', 'donate', 'sedekah', 'qris', 'payment'],
     category: 'main',
     noPrefix: true,
     call: async (conn, m, { usedPrefix, command }) => {
         const namabt = global.namebot;
         let caption = `*☕ Support ${namabt} ☕*\n\n`;
-        caption += `Halo Kak! Terima kasih sudah menggunakan ${namabt}. Jika kakak ingin membantu biaya maintenance server dan pengembangan bot ini, bisa melalui QRIS di atas ya!\n\n`;
-        caption += `*Bisa lewat:* \n`;
-        caption += `• Dana / OVO / GoPay / LinkAja / Dll\n`;
-        caption += `• All Bank (Scan via Mobile Banking)\n\n`;
-        caption += `_Berapapun donasinya, sangat berarti buat kelangsungan hidup bot ini. Terima kasih!_ `;
-
-        // Ambil link dari global.qris
+        caption += `Halo! Terima kasih sudah menggunakan ${namabt}. Jika kamu ingin membantu biaya maintenance server, pengembangan bot ini atau bayar premium/sewa, bisa melalui QRIS ini ya. Suppurt All Payment!\n\n`;
         const qrisUrl = global.qris; 
 
         try {
             if (qrisUrl) {
-                await conn.sendMessage(m.chat, { 
-                    image: { url: qrisUrl }, 
-                    caption: caption 
-                }, { quoted: m });
+                await conn.sendMessage(m.chat, {
+                image: { url: qrisUrl },
+                caption: caption,
+                contextInfo: {
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: idch,
+                        newsletterName: namech,
+                        serverMessageId: 143
+                    }
+                }
+            }, { quoted: m });
             } else {
                 // Jika global.qris kosong
                return m.reply(caption + `\n\n*(Ssst, Owner belum setting link QRIS di config!)*`);
