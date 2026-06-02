408f06f0f09a566c4b2497edffb49f914fa32517
Yushima <aditwibubawang@gmail.com>
2026-06-02 15:30:52 +0700

    Update owner-getplugins.js

--- plugins/owner-getplugins.js
+++ plugins/owner-getplugins.js
@@ -9,32 +9,33 @@ const path = require('path');
 module.exports = {
     command: ['gp'],
     category: 'owner',
-    owner: true, // WAJIB OWNER! Bahaya kalau user biasa bisa intip session atau apikey
+    owner: true,
     call: async (conn, m, { text, usedPrefix, command }) => {
-        if (!text) return m.reply(`Mau ambil plugin apa? 🗿\nContoh: *${usedPrefix + command} smeme.js*`);
+        if (!text) return m.reply(`> Mau ambil plugin apa?\nContoh: *${usedPrefix + command} smeme.js*`);
 
-        // Pastikan input nama file benar
         const filename = text.endsWith('.js') ? text : `${text}.js`;
         const pathPlugin = path.join(__dirname, filename);
 
         try {
-            // 1. Cek apakah filenya ada
             if (!fs.existsSync(pathPlugin)) {
-                return m.reply(`❌ File *${filename}* tidak ditemukan di folder plugins!`);
+                return m.reply(`> File *${filename}* tidak ditemukan di folder plugins!`);
             }
 
-            // 2. Baca isi file
             const content = fs.readFileSync(pathPlugin, 'utf-8');
 
-            // 3. Kirim hasilnya
-            // Kita bungkus pakai backtick (```) biar tampilannya jadi blok kode di WA
-            let caption = `⛩️ *PLUGIN SOURCE CODE* ⛩️\n\n`;
-            caption += `📂 *File:* ${filename}\n`;
-            caption += `📏 *Size:* ${fs.statSync(pathPlugin).size} bytes\n\n`;
-            caption += `\`\`\`javascript\n${content}\n\`\`\``;
+            let caption = `${content}`;
 
-            await m.reply(caption);
-
+            await conn.sendMessage(m.chat, {
+                text: caption,
+                    contextInfo: {
+                        isForwarded: true,
+                        forwardedNewsletterMessageInfo: {
+                            newsletterJid: idch,
+                            newsletterName: namech,
+                            serverMessageId: 143
+                        }
+                    }
+                }, { quoted: m });
         } catch (e) {
             console.error(e);
             m.reply(`❌ *Gagal mengambil plugin:* ${e.message}`);
