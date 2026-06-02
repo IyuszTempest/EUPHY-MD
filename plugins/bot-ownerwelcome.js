56672b1623d73656d3afba6dd5e9d25a5d07047a
Yushima <aditwibubawang@gmail.com>
2026-06-02 14:20:02 +0700

    Update bot-ownerwelcome.js

--- plugins/bot-ownerwelcome.js
+++ plugins/bot-ownerwelcome.js
@@ -4,32 +4,34 @@
  */
 
 module.exports = {
-    // Menggunakan properti before untuk menangani setiap pesan masuk
     before: async function (m, { conn }) {
-        if (!m.isGroup) return; // Hanya aktif di grup
-        if (m.fromMe) return;   // Abaikan jika bot yang kirim pesan
+        if (!m.isGroup) return; 
+        if (m.fromMe) return;  
 
-        // Identitas Owner (Nomor kamu sendiri)
-        const ownerNumber = global.targetjid;
+        const ownerNumber = global.lidowner;
         
-        // Cek apakah pengirim adalah Owner
         if (m.sender !== ownerNumber) return;
 
-        // Inisialisasi database user jika belum ada
         let user = global.db.data.users[m.sender] || {};
         let now = +new Date();
 
-        // Cooldown: Hanya menyapa 1 jam sekali agar tidak spam
-        if (user.ownerWelcome && now - user.ownerWelcome < 3600000) return;
+        if (user.ownerWelcome && now - user.ownerWelcome < 3600000) return; //milidetik
 
-        // Update waktu sambutan terakhir di database
         user.ownerWelcome = now;
         global.db.data.users[m.sender] = user;
 
-        // Mengirim pesan sambutan
+        
         await conn.sendMessage(m.chat, {
-            text: `Waspada, ownerku dah datang.\n@${ownerNumber.split('@')[0]}`,
-            mentions: [ownerNumber]
-        }, { quoted: m });
+                    text: `Waspadalah, sosok owner dah datang.\n@${ownerNumber.split('@')[0]} 😱`,
+                    mentions: [ownerNumber],
+                    contextInfo: {
+                        isForwarded: true,
+                        forwardedNewsletterMessageInfo: {
+                            newsletterJid: idch,
+                            newsletterName: namech,
+                            serverMessageId: 143
+                        }
+                    }
+                }, { quoted: m });
     }
 };
